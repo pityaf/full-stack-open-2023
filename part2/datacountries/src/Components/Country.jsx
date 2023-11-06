d922034e1f777e90f95ef627024930a57d457fba
Paolo Cantoreggi <paolo@DESKTOP-PAOLO>
2023-11-06 23:39:41 +0100

    add weather options

--- part2/datacountries/src/Components/Country.jsx
+++ part2/datacountries/src/Components/Country.jsx
@@ -1,4 +1,14 @@
+import Weather from "./Weather";
+
 const Country = ({ country }) => {
+
+    const flag = {
+        borderColor: 'black',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        marginRight: 5,
+    };
+
     if(Object.keys(country).length === 0 && country.constructor === Object) {
         return;
     }
@@ -9,20 +19,25 @@ const Country = ({ country }) => {
     const plural = 'Languages:';
 
     return(
-        <div>
-            <h2>{ country.name.common }</h2>
-            <p>Capital: { country.capital[0] }</p>
-            <p>Area: { country.area }</p>
-            <h3>
-                { langs.length === 1 ? singular : plural}
-            </h3>
-            <ul>
-                { 
-                    langs.map(key => <li key={ key }>{ country.languages[key] }</li>)
-                }
-            </ul>
-            <p>Timezone: { country.timezones[0] }</p>
-            <img src={ country.flags.png } alt='coutry-flag' height={200} width={300}/>
+        <div style={{ display:'flex' }}>
+            <div style={{display: 'flex', alignItems: 'center', backgroundColor: '#CDCDCD', paddingLeft: 20}}>
+                <img style={ flag } src={ country.flags.png } alt='coutry-flag' height={210} width={300}/>
+            </div>
+            <div style={{paddingLeft: 20, backgroundColor: '#CDCDCD'}}>
+                <h2>{ country.name.common }</h2>
+                <p>Capital: { country.capital[0] }</p>
+                <p>Area: { country.area }</p>
+                <h3>
+                    { langs.length === 1 ? singular : plural}
+                </h3>
+                <ul>
+                    { 
+                        langs.map(key => <li key={ key }>{ country.languages[key] }</li>)
+                    }
+                </ul>
+                <p>Timezone: { country.timezones[0] }</p>
+            </div>
+            <Weather capital={ country.capital[0] }/>
         </div>
     )
 }
