bf6cf81a838f74d0624589c5880b60feef28b5c6
Paolo Cantoreggi <paolo@DESKTOP-PAOLO>
2023-12-17 01:22:54 +0100

    add 3.22 functionalities

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -8,61 +8,29 @@ const Person = require('./models/person');
 
 const app = express();
 
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-  }
+const requestLogger = (request, _response, next) => {
+    console.log('Method:', request.method);
+    console.log('Path:  ', request.path);
+	console.log('Body:  ', request.body);
+    console.log('---');
+    next();
+  };
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(morgan('tiny'));
-app.use(requestLogger)
+app.use(requestLogger);
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
-
-const generateId = () => {
-    return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-}
-
-const duplicate = (person) => {
-    return persons.find(per => per.name === person.name);
-}
-
-
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
     response.send('<h1>Server Working!</h1>');
-})
+});
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (_request, response) => {
     Person.find({}).then(persons => {
         response.json(persons);
-    })
-})
+    });
+});
 
 app.get('/api/persons/:id', (request, response, next) => {
     Person
@@ -76,21 +44,20 @@ app.get('/api/persons/:id', (request, response, next) => {
        
         })
         .catch(error => next(error));
-})
+});
 
-app.get('/info', (request, response) => {
+app.get('/info', (_request, response) => {
     Person.find({}).then(persons => {
         const info = `<p>Phonebook hs info for ${ persons.length } people</p>`;
         const date = `<p>${ new Date }</p>`;
-
         response.send(info + date);
-    })
-})
+    });
+});
 
 morgan.token('body', request => JSON.stringify(request.body));
-const postMorganLog = morgan(':method :url :status :res[content-length] - :response-time ms :body')
+const postMorganLog = morgan(':method :url :status :res[content-length] - :response-time ms :body');
 
-app.post('/api/persons', postMorganLog, (request, response) => {
+app.post('/api/persons', postMorganLog, (request, response, next) => {
     const body = request.body;
 
     if(!body) {
@@ -107,65 +74,58 @@ app.post('/api/persons', postMorganLog, (request, response) => {
         const person = new Person ({
             name: body.name,
             number: body.number,
-        })
+        });
     
-        if(duplicate(person)) {
-            response.status(400).json({
-                'error': 'name must be unique',
-            }).end();
-        } else {
-            person.save().then(savedPerson => {
-                response.json(savedPerson);
-            })
-        }
+        person.save().then(savedPerson => {
+            response.json(savedPerson);
+        })
+        .catch(error => next(error));
     }
-})
+});
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body;
+    const { name, number} = request.body;
 
-    console.log('updating...')
+    console.log('updating...');
 
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
-
-    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
         .then(updatedPerson => {
             response.json(updatedPerson);
         })
         .catch(error => next(error));
-})
+});
 
 
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
-        .then(restult => {
+        .then(() => {
             response.status(204).end();
         })
         .catch(error => next(error));
-})
+});
 
 
-const unknownEndpoint = (request, response) => {
+const unknownEndpoint = (_request, response) => {
     response.status(404).send({
         'error': 'unknown endpoint',
     });
-}
+};
 app.use(unknownEndpoint);
 
-const errorHandler = (error, request, response, next) => {
+const errorHandler = (error, _request, response, next) => {
     console.log(error.message);
 
     if(error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id'});
+    } else if(error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message });
     }
 
     next(error);
-}
+};
 app.use(errorHandler);
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${ PORT }`);
--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -2,31 +2,45 @@ const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
 
-const password = process.argv[2];
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI;
 
 console.log('connecting to', url);
 
 mongoose
     .connect(url)
-    .then(result => {
+    .then(() => {
         console.log('connected to MongoDB');
     })
     .catch(error => {
         console.log('error connecting to MongoDB:', error.message);
-    })
+    });
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-})
+    name: {
+        type: String,
+        minLength: 3,
+        required: true,
+    },
+    number: {
+        type: String,
+        validate: {
+            validator: function(v) {
+              return /^\d{2,3}-\d{6,}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number! Two parts required separated by -, the first part has two or three numbers and the second part also consists of numbers`,
+          },
+          minLength: 8,
+          required: [true, 'User phone number required'],
+    },
+});
 
 personSchema.set('toJSON', {
-        transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
+        transform: (_document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
         }
-    })
+    });
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema);
